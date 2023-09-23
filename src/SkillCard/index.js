3cf949072cb75112799e98f1d6d66f7c98e5b039
vk1018 <vamsisanapala888@gmail.com>
2023-09-23 05:07:11 +0000

    jobby

---
+++ src/SkillCard/index.js
@@ -0,0 +1,13 @@
+const SkillCard = props => {
+  const {skillDetails} = props
+  const {imageUrl, name} = skillDetails
+  return (
+    <li className="skill-item-container">
+      <div className="skill-container">
+        <img src={imageUrl} alt={name} className="skill-image" />
+        <p className="skill-name">{name}</p>
+      </div>
+    </li>
+  )
+}
+export default SkillCard
---
+++ src/components/Home/index.js
@@ -0,0 +1,25 @@
+import {Link} from 'react-router-dom'
+import Header from '../Header'
+
+import './index.css'
+
+const Home = () => (
+  <div>
+    <Header />
+    <div className="home-container">
+      <div className="home-content">
+        <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+        <p className="home-description">
+          Millions of people are searching for jobs,salary information, company
+          reviews. Find the jobs that fits your abilities and potential.
+        </p>
+        <Link to="/jobs">
+          <button type="button" className="shop-now-button">
+            Find Jobs
+          </button>
+        </Link>
+      </div>
+    </div>
+  </div>
+)
+export default Home
